aeaf8461487b163da7f9562aa2facfd16fa8529f
I'm <66580267+quys-nt@users.noreply.github.com>
2023-10-12 10:12:31 +0000

    triangle Equi 2

--- assets/scripts.js
+++ assets/scripts.js
@@ -16,10 +16,45 @@ function triangleRevert(num) {
     if (i !== 1) {
       star += "\n"
     }
-
   }
   return star
 }
 
-// console.log(triangleRevert(10));
+function triangleEqui2(num) {
+  let result = "";
+
+  for (let i = 0; i < num; i++) {
+    let space = ' '.repeat(i);
+    let stars = '*'.repeat(num - i);
+
+    result += space + stars;
+
+    if (i < num - 1) {
+      result += '\n';
+    }
+  }
+
+  return result;
+
+}
+
+function triangleEqui(num) {
+  let result = "";
+  for (let i = num; i >= 1; i--) {
+    let star = "";
+    if (i % 2 != 0) {
+      let space = ' '.repeat((num - i) / 2);
+      for (let j = i; j >= 1; j--) {
+        star += "*";
+      }
+      result += space += star;
+      if (i !== 1) {
+        result += "\n"
+      }
+    }
+  }
+  return result
+}
+
+console.log(triangleEqui(99));
 
