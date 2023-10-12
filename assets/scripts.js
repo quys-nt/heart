dbd0d60cae4f45106ffeaaf68b89fac03406a02f
I'm <66580267+quys-nt@users.noreply.github.com>
2023-10-12 08:43:12 +0000

    function triangle revert

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,5 +5,21 @@ function triangle(number) {
     console.log(star);
   }
 }
+// triangle(5)  
 
-triangle(5);
+function triangleRevert(num) {
+  let star = ""
+  for (let i = num; i >= 1; i--) {
+    for (let j = i; j >= 1; j--) {
+      star += "*";
+    }
+    if (i !== 1) {
+      star += "\n"
+    }
+
+  }
+  return star
+}
+
+// console.log(triangleRevert(10));
+
